83743b28792a0f72f35da53e215b7fed1c716729
Slava Fir <slava.fir@gmail.com>
2019-07-11 15:16:15 +0300

    currency extended to use not only atoms but strings too

--- lib/currency.ex
+++ lib/currency.ex
@@ -10,8 +10,14 @@ defmodule Dinero.Currency do
     @currencies
   end
 
-  def get!(currency) do
-    Enum.find(all(), fn map -> map[:code] == currency end) ||
+  def get!(currency) when is_atom(currency) do
+    currency
+    |>Atom.to_string
+    |>get!
+  end
+
+  def get!(currency) when is_binary(currency) do
+    Enum.find(all(), fn map -> map[:code] |> Atom.to_string |> String.downcase == String.downcase(currency) end) ||
       raise(ArgumentError, "currency #{currency} not found")
   end
 end
--- test/currency_test.exs
+++ test/currency_test.exs
@@ -13,7 +13,9 @@ defmodule CurrencyTest do
 
   test "get!" do
     assert Currency.get!(:USD) == %{name: "United States dollar", code: :USD}
-    assert_raise ArgumentError, fn -> Currency.get!(:usd) end
+    assert Currency.get!(:usd) == %{name: "United States dollar", code: :USD}
+    assert Currency.get!(:Usd) == %{name: "United States dollar", code: :USD}
+    assert Currency.get!("usd") == %{name: "United States dollar", code: :USD}
     assert_raise ArgumentError, fn -> Currency.get!(:ABC) end
   end
 end
